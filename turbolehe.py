ae1cc32555b51e49a9b39b8e67ba4dbc67774608
UserCr4ig <87878066+UserCr4ig@users.noreply.github.com>
2023-12-27 15:22:35 +0100

    code translated in english

--- turbolehe.py
+++ turbolehe.py
@@ -3,27 +3,26 @@ import csv
 import os
 import argparse
 
-# Paramètres
+# Parameters
 domains = ["@gmail.com", "@yahoo.com", "@hotmail.com", "@outlook.com"]
 
-# Analyse des arguments de ligne de commande
+# Command line arguments parsing
 parser = argparse.ArgumentParser()
-parser.add_argument('search_term', nargs='+', help='Nom prénom ou chaîne de caractères')
-parser.add_argument('-B', action='store_true', help='Option -B pour générer uniquement des adresses de domaine spécifié')
+parser.add_argument('search_term', nargs='+', help='First name, last name, or string to search for')
+parser.add_argument('-B', action='store_true', help='Option -B to generate only specified domain addresses')
 args = parser.parse_args()
 
-# Vérifier la validité du terme de recherche
+# Check the validity of the search term
 search_term = ' '.join(args.search_term)
 if not search_term.replace(' ', '').isalpha():
-    print("Erreur : Le terme de recherche doit contenir uniquement des lettres alphabétiques.")
+    print("Error: The search term must contain only alphabetical letters.")
     exit()
 
-
-# Fonction pour générer des adresses e-mail probables
-def generer_adresses(search_term, domains):
-    adresses = []
+# Function to generate probable email addresses
+def generate_addresses(search_term, domains):
+    addresses = []
     first_name, last_name = search_term.split(' ', 1)
-    
+
     for domain in domains:
         email_formats = [
             first_name.lower() + '.' + last_name.lower() + domain,
@@ -37,71 +36,70 @@ def generer_adresses(search_term, domains):
             last_name.lower() + first_name.lower() + domain,
             first_name.lower() + '.' + last_name.lower()[0] + domain,
         ]
-        
-        adresses.extend(email_formats)
 
-    return adresses
+        addresses.extend(email_formats)
 
-# Générer les adresses e-mail probables en fonction du terme de recherche
-adresses_email = generer_adresses(search_term,domains)
+    return addresses
 
-# Vérifier si l'option -B est spécifiée pour filtrer les adresses par nom de domaine
+# Generate probable email addresses based on the search term
+email_addresses = generate_addresses(search_term, domains)
+
+# Check if the -B option is specified to filter addresses by domain name
 if args.B:
-    domain = input("Entrez le nom de domaine : ")
-    adresses_email = [adresse for adresse in adresses_email if adresse.endswith(domain)]
+    domain = input("Enter the domain name: ")
+    email_addresses = [address for address in email_addresses if address.endswith(domain)]
 
-# Création arboresence
+# Create directory structure
 first_name, last_name = search_term.split(' ', 1)
-commande_arboresence = f"mkdir {first_name}-{last_name}"
-subprocess.run(commande_arboresence, shell=True)
+directory_command = f"mkdir {first_name}-{last_name}"
+subprocess.run(directory_command, shell=True)
 
-# Exécution de la commande pour tester chaque adresse e-mail
-for adresse in adresses_email:
-    commande_holehe = f"cd {first_name}-{last_name} && holehe {adresse} -C"
-    subprocess.run(commande_holehe, shell=True)
-    repertoire = os.path.join(os.getcwd(), f"{first_name}-{last_name}")
+# Execute the command to test each email address
+for address in email_addresses:
+    command_holehe = f"cd {first_name}-{last_name} && holehe {address} -C"
+    subprocess.run(command_holehe, shell=True)
+    directory = os.path.join(os.getcwd(), f"{first_name}-{last_name}")
 
-# Liste pour stocker les noms de fichiers valides
-fichiers_valides = []
+# List to store valid file names
+valid_files = []
 
-# Parcourir tous les fichiers dans le répertoire
-for fichier in os.listdir(repertoire):
-    if fichier.endswith(".csv") and fichier.startswith("holehe_"):
-        # Ajouter le nom de fichier valide à la liste
-        fichiers_valides.append(fichier)
+# Iterate through all files in the directory
+for file in os.listdir(directory):
+    if file.endswith(".csv") and file.startswith("holehe_"):
+        # Add the valid file name to the list
+        valid_files.append(file)
 
-# Chemin du fichier de sortie
-chemin_sortie = os.path.join(repertoire, 'main.csv')
+# Output file path
+output_path = os.path.join(directory, 'main.csv')
 
-# Ouvrir le fichier de sortie en mode écriture
-with open(chemin_sortie, 'w', newline='') as csvfile:
+# Open the output file in write mode
+with open(output_path, 'w', newline='') as csvfile:
     writer = None
 
-    # Parcourir les fichiers valides
-    for fichier in fichiers_valides:
-        # Chemin du fichier CSV
-        chemin_fichier = os.path.join(repertoire, fichier)
+    # Iterate through valid files
+    for file in valid_files:
+        # CSV file path
+        file_path = os.path.join(directory, file)
 
-        # Extraire l'adresse e-mail à partir du nom de fichier
-        adresse_mail = fichier.split("_")[2]
+        # Extract email address from the file name
+        email_address = file.split("_")[2]
 
-        # Ouvrir le fichier CSV en mode lecture
-        with open(chemin_fichier, newline='') as csvfile_in:
+        # Open the CSV file in read mode
+        with open(file_path, newline='') as csvfile_in:
             reader = csv.DictReader(csvfile_in)
             fieldnames = reader.fieldnames
 
-            # Ajouter la colonne 'adresse mail' en première position
-            fieldnames.insert(0, 'adresse mail')
+            # Add the 'email address' column in the first position
+            fieldnames.insert(0, 'email address')
 
-            # Créer le writer si ce n'est pas déjà fait
+            # Create the writer if not already done
             if writer is None:
                 writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
                 writer.writeheader()
 
-            # Lire chaque ligne du fichier CSV et l'écrire dans le fichier de sortie avec l'adresse mail correspondante
+            # Read each row from the CSV file and write it to the output file with the corresponding email address
             for row in reader:
-                row['adresse mail'] = adresse_mail
+                row['email address'] = email_address
                 writer.writerow(row)
 
-
-print("Fichier main.csv généré avec succès.")
+print("Successfully generated main.csv file.")
